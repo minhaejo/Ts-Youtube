b3b874c3ff64ae5211ba5dd280ac38161768748c
minhaejo <robo888@naver.com>
2022-08-07 20:16:39 +0900

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { FC, useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import VideoList from "./components/VideoList/VideoList";
+import { type } from "@testing-library/user-event/dist/type";
 
+//특정 데이터 뽑아서 맵 돌릴거임 그 후 컴포넌트 랜더링 시킬거임
+//이때 타입스크립트에서 타입정의를 어캐해야할지 고민됨
+//기존 데이터에 몇개만 설정해서 해보니 정상적으로 됐음 하지만 데이터의 뎁스가 깊어서 타입정의에 있어 문제가됐음
+//데이터를 가져오는 과정에서 어떻게 타입선언을 해야하는지 콘솔에 표기되는 몇십개의 모든 데이터를 모두 정의해야하는지?
+//그러려고 해도 각 컨텐츠별로 데이터가 달랐음
+//이런문제를 어떤 사고로 접근해야하는지?
 function App() {
+  const [vedios, setVedios] = useState([]);
+
+  useEffect(() => {
+    const requestOptions: RequestInit = {
+      method: "GET",
+      redirect: "follow",
+    };
+
+    fetch(
+      "https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyCy-Iqm1Qrw3KNbTZ_CrE6PnG8RZUpXpPk",
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) => setVedios(result.items))
+      .catch((error) => console.log("error", error));
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <VideoList />
     </div>
   );
 }
---
+++ src/components/VideoList/VideoList.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from "react";
+import VideoItem from "../VideoItem/VideoItem";
+
+const VideoList = () => {
+  return (
+    <>
+      <ul></ul>
+    </>
+  );
+};
+
+export default VideoList;
